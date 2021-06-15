4f308463abf01c1516c50ff6d0de82b6a899d670
Maximilian Walter <maximilian.walter@kit.edu>
2021-06-15 09:10:51 +0200

    fixed build dependency

--- bundles/org.palladiosimulator.pcm.confidentiality.context.editor.sirius.design/src/org/palladiosimulator/pcm/confidentiality/context/editor/sirius/design/Activator.java
+++ bundles/org.palladiosimulator.pcm.confidentiality.context.editor.sirius.design/src/org/palladiosimulator/pcm/confidentiality/context/editor/sirius/design/Activator.java
@@ -18,7 +18,7 @@ public class Activator extends AbstractUIPlugin {
     // The shared instance
     private static Activator plugin;
 
-    private static Set<Viewpoint> viewpoints; 
+    private static Set<Viewpoint> viewpoints;
 
     /**
      * The constructor
@@ -28,39 +28,42 @@ public class Activator extends AbstractUIPlugin {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
      */
-    public void start(BundleContext context) throws Exception {
-      super.start(context);
-	  plugin = this;
-	  viewpoints = new HashSet<Viewpoint>();
-	  viewpoints.addAll(ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/contextConfidentialityEditor.odesign")); 
+    @Override
+    public void start(final BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+        viewpoints = new HashSet<Viewpoint>();
+        viewpoints.addAll(ViewpointRegistry.getInstance()
+                .registerFromPlugin(PLUGIN_ID + "/description/contextConfidentialityEditor.odesign"));
     }
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
      */
-    public void stop(BundleContext context) throws Exception {
-	plugin = null;
-	if (viewpoints != null) {
-	    for (final Viewpoint viewpoint: viewpoints) {
-		ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
-	    }
-	    viewpoints.clear();
-	    viewpoints = null; 
-	}
-	super.stop(context);
+    @Override
+    public void stop(final BundleContext context) throws Exception {
+        plugin = null;
+        if (viewpoints != null) {
+            for (final Viewpoint viewpoint : viewpoints) {
+                ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
+            }
+            viewpoints.clear();
+            viewpoints = null;
+        }
+        super.stop(context);
     }
 
     /**
      * Returns the shared instance
-     * 
+     *
      * @return the shared instance
      */
     public static Activator getDefault() {
-	return plugin;
+        return plugin;
     }
 }
